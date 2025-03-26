8350b68058d7890890558bc8b812038ae9bbd872
Favour  Emeka <favorodera@gmail.com>
2025-03-26 21:14:26 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -25,13 +25,13 @@ useSeoMeta({
   ogImage: {
     alt: 'Favour Emeka',
     type: 'image/jpeg',
-    url: 'https://favorodera.vercel.app/_vercel/image?url=%2Favatar.jpeg&w=1536&q=100',
+    url: 'https://favorodera.vercel.app/_vercel/image?url=%2Favatar.jpg&w=1536&q=100',
     height: '630',
     width: '1200',
   },
   twitterImage: {
     alt: 'Favour Emeka',
-    url: 'https://favorodera.vercel.app/_vercel/image?url=%2Favatar.jpeg&w=1536&q=100',
+    url: 'https://favorodera.vercel.app/_vercel/image?url=%2Favatar.jpg&w=1536&q=100',
   },
   profileGender: 'Male',
   ogType: 'profile',
