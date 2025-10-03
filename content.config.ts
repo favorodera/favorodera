33206c4e6cae82588a22c4f2ded83364bb02a2f9
Favour  Emeka <favorodera@gmail.com>
2025-10-03 15:55:00 +0100

    feat: enhance app configuration and UI components; add contact component and update styles

--- content.config.ts
+++ content.config.ts
@@ -2,6 +2,9 @@ import { defineContentConfig, defineCollection } from '@nuxt/content'
 
 export default defineContentConfig({
   collections: {
-    
+    home: defineCollection({
+      source: 'index.md',
+      type: 'page',
+    }),
   },
 })
