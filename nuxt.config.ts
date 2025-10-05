9eddfbc56bc873d89b32d1ad8b8c9db1b41d31da
Favour  Emeka <favorodera@gmail.com>
2025-10-05 19:00:15 +0100

    feat: enhance SEO and app configuration; add @nuxtjs/seo module, update title template, and improve meta tags in app.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,26 +3,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   extends: ['@favorodera/nuxt-helper'],
-  modules: [
-    '@nuxt/ui',
-    '@vueuse/nuxt',
-    '@nuxt/eslint',
-    '@nuxt/content',
-  ],
+  modules: ['@nuxt/ui', '@vueuse/nuxt', '@nuxt/eslint', '@nuxt/content', '@nuxtjs/seo'],
   imports: {
     dirs: ['./app/stores'],
   },
   devtools: { enabled: true },
   app: {
     rootTag: 'main',
-    head: {
-      htmlAttrs: { lang: 'en', dir: 'ltr' },
-      link: [
-        { rel: 'icon', href: '/favicon.ico' },
-        { rel: 'icon', href: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-        { rel: 'icon', href: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      ],
-    },
     rootAttrs: {
       id: 'app',
     },
@@ -30,6 +17,10 @@ export default defineNuxtConfig({
     layoutTransition: { name: 'fade-out-in', mode: 'out-in' },
   },
   css: ['~/assets/styles/index.css'],
+  site: {
+    url: 'https://favorodera.vercel.app/',
+    name: 'Favour Emeka',
+  },
   content: {
     preview: {
       api: 'https://api.nuxt.studio',
