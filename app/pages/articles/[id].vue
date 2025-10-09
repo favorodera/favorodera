ae34734699a6a5e8fc453dcf12156a6aa3b35f44
Favour  Emeka <favorodera@gmail.com>
2025-10-09 09:46:55 +0100

    feat: enhance article page layout and functionality; add Table of Contents component, improve UI structure, and update TypeScript article content for better readability

--- app/pages/articles/[id].vue
+++ app/pages/articles/[id].vue
@@ -1,10 +1,27 @@
 <template>
 
-  <UPage>
+  <UPage
+    :ui="{
+      root: 'flex flex-col lg:grid-cols-[minmax(0,256px)_48rem_minmax(0,256px)] xl:grid xl:grid-cols-[minmax(0,256px)_48rem_minmax(0,256px)] xl:gap-8 mx-auto w-fit',
+      center: 'lg:col-span-[48rem] lg:col-start-2 xl:col-span-[48rem] xl:col-start-2',
+      right: 'lg:col-span-1 xl:col-span-1 order-first lg:order-last xl:order-last',
+    }"
+  >
 
-    <UContainer>
+    <UContainer class="lg:px-0">
 
       <template v-if="page">
+
+        <UContentToc
+          v-if="page.body?.toc?.links?.length"
+          title="Table of Contents"
+          :links="page.body.toc.links"
+          :ui="{
+            root: 'xl:hidden xl:bg-[initial]',
+            container: 'lg:py-0 lg:pt-6 lg:pb-4.5 lg:border-b xl:border-0 [&>button]:lg:flex [&>button]:cursor-pointer [&>p]:lg:hidden [&>div:first-of-type]:lg:!flex [&>div:last-of-type]:lg:!hidden ',
+            trailingIcon: 'lg:inline',
+          }"
+        />
 
         <UPageHeader
           :title="page.title"
@@ -22,9 +39,11 @@
         </UPageHeader>
 
         <UPageBody>
+       
           <ContentRenderer :value="page" />
 
-          <AppSurround :left="{ path: '/', text: 'Back to Home' }" />
+          <AppSurround :left="{ path: '/articles', text: 'Back to Articles' }" />
+
         </UPageBody>
 
       </template>
@@ -38,11 +57,22 @@
           message: error?.message || 'Something went wrong!',
         }"
       />
-   
 
     </UContainer>
 
-
+    <template
+      v-if="page?.body?.toc?.links?.length"
+      #right
+    >
+      <UContentToc
+        title="Table of Contents"
+        :links="page.body.toc.links"
+        :ui="{
+          root: 'max-xl:hidden xl:p-0 -mx-0 sm:-mx-0 xl:m-0 xl:pr-8',
+          container: 'xl:p-0',
+        }"
+      />
+    </template>
   </UPage>
   
 </template>
