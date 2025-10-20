82bdaa18653d3767ef8e460c0a3ae8207a853d3a
Favour  Emeka <favorodera@gmail.com>
2025-10-20 17:56:32 +0100

    feat: update bookmark component to display descriptions and improve styling; enhance bookmarks.json with new descriptions for better context

--- app/components/bookmark/item.vue
+++ app/components/bookmark/item.vue
@@ -5,15 +5,17 @@
     :to="url"
     external
     active
-    class="group flex flex-col justify-between gap-x-4 gap-y-2 rounded-md p-2 transition-all hover:bg-muted/50 sm:flex-row sm:items-center"
-    :title="description"
+    class="group flex flex-col justify-between gap-x-8 gap-y-2 rounded-md p-2 transition-all hover:bg-muted/50"
+    :title="url"
   >
 
-    <h2 class="line-clamp-1 flex-1 font-medium tracking-tight group-hover:text-secondary">
+    <h2 class="line-clamp-1 min-w-0 truncate font-medium tracking-tight group-hover:text-secondary ">
       {{ title }}
     </h2>
 
-    <span class="line-clamp-1 text-xs text-muted">{{ url }}</span>
+    <span class="line-clamp-2 text-xs text-muted">
+      {{ description }}
+    </span>
 
   </ULink>
 
